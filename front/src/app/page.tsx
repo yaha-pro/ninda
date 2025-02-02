d543a818e1838eaa99898faad2be9cd1b44036d3
yaha <saxsax0404@gmail.com>
2025-02-02 14:47:06 +0900

    add:トップページ（Hero）を追加しました

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,17 @@
 // import Link from "next/link";
-import { title } from "process";
 
 export default function Home() {
   return (
-    title
+    <div className="sm:px-6 lg:py-12 bg-cover bg-center bg-[url('/hero_image.jpeg')]">
+      <div className="text-center mb-16">
+        <div className="inline-block rounded-full mb-8">
+          <img src="ninda-hero-logo.png" alt="忍打" className="w-80" />
+        </div>
+        <p className="max-w-2xl mx-auto">
+          「忍打」は、「忍者のように縦横かつ素早くタイピングができるようになろう！」というコンセプトのタイピングゲームサービスです。
+          自身でオリジナルのタイピング問題を作成することができるため、苦手な文章の問題を作成して練習したり、他のユーザーが作ったタイピング問題を選んでタイピングの速さを競うことができます。
+        </p>
+      </div>
+    </div>
   );
 }
--- front/src/components/ui/HamburgerMenu.tsx
+++ front/src/components/ui/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 export function HamburgerMenu() {
@@ -8,31 +8,24 @@ export function HamburgerMenu() {
         <RxHamburgerMenu className="text-red-500 mt-1 w-10 h-10 hover:opacity-60 transition-opacity duration-300" />
       </SheetTrigger>
       <SheetContent side="right" className="w-64 bg-[#faf7ef] font-bold">
+        <SheetHeader>
+          <SheetTitle></SheetTitle>
+        </SheetHeader>
         <nav className="mt-4 space-y-4 text-gray-600">
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            新規登録/ログイン
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            使い方
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            投稿一覧
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            ユーザー一覧
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            タグ一覧
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            利用規約
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            プライバシー
-          </a>
-          <a href="#" className="block px-4 py-2 text-lg hover:underline">
-            お問い合わせ
-          </a>
+          {[
+            "新規登録/ログイン",
+            "使い方",
+            "投稿一覧",
+            "ユーザー一覧",
+            "タグ一覧",
+            "利用規約",
+            "プライバシー",
+            "お問い合わせ",
+          ].map((item) => (
+            <a key={item} href="#" className="block px-4 py-2 text-lg hover:underline">
+              {item}
+            </a>
+          ))}
         </nav>
       </SheetContent>
     </Sheet>
